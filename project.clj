d9f765ab1590dc4f414c2362aad0b992148d9fe3
Faris Nasution <nasution.faris@gmail.com>
2015-02-25 17:12:07 +0700

    Update [clojurescript] and [sablono] version

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]
+                 [org.clojure/clojurescript "0.0-2913"]
                  [figwheel "0.2.2-SNAPSHOT"]
                  [hiccup "1.0.5"]
                  [org.omcljs/om "0.8.8"]
-                 [sablono "0.3.3"]
+                 [sablono "0.3.4"]
                  [secretary "1.2.1"]]
   :plugins [[lein-cljsbuild "1.0.4"]
             [lein-figwheel "0.2.2-SNAPSHOT"]
