8c723ad2efb02043ed5f9545f8b75ddae60c43b0
Faris Nasution <nasution.faris@gmail.com>
2015-02-28 13:51:57 +0700

    Fix url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject katka "0.1.0"
   :description "Finals"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/farisnasution/katka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
