778dc2323f0f382d5b69882090873a140f3af4e4
Faris Nasution <nasution.faris@gmail.com>
2015-03-05 13:19:50 +0700

    Refactor bar element.

--- src/cljs/katka/chart/bar.cljs
+++ src/cljs/katka/chart/bar.cljs
@@ -2,180 +2,149 @@
   (:require [om.core :as om :include-macros true]
             [sablono.core :as html :refer-macros [html]]
             [katka.shape :as shape]
-            [katka.chart.axis :as axis]
-            [katka.scale.ordinal :as osc]
+            [katka.chart.axis :as ax]
             [katka.scale.linear :as lsc]
+            [katka.scale.ordinal :as osc]
             [katka.util.math :as math]
             [katka.util.data :as data])
   (:use-macros [katka.macro :only [defcomponent]]))
 
-(defcomponent vertical-rects
-  "Creates a series of React rectangle element.
+(defn construct-width
+  [scale rect-type]
+  (if (= rect-type "horizontal")
+    (fn [[_ d]]
+      (-> (scale d)
+          (- (scale 0))
+          math/->pos))
+    (fn [_] (.rangeBand scale))))
 
-   Accept:
+(defn construct-height
+  [scale rect-type]
+  (if (= rect-type "horizontal")
+    (fn [_] (.rangeBand scale))
+    (fn [[_ d]]
+      (-> (scale d)
+          (- (scale 0))
+          math/->pos))))
 
-   {:g {:pos-x <num>
-        :pos-y <num>}
-    :scale {:padding <num>
-            :width <num>
-            :height <num>}
-    :style {:fill <string>
-            :stroke <string>}
-    :data <vector of map>}"
-  [{:keys [g scale style data]} owner]
-  (display-name [_] "vertical-rects")
+(defn construct-x
+  [scale rect-type]
+  (if (= rect-type "horizontal")
+    (fn [[_ d]]
+      (if (neg? d)
+        (scale 0)
+        (scale d)))
+    (fn [[d _]] (scale d))))
+
+(defn construct-y
+  [scale rect-type]
+  (if (= rect-type "horizontal")
+    (fn [[d _]] (scale d))
+    (fn [[_ d]]
+      (if (neg? d)
+        (scale 0)
+        (scale d)))))
+
+(defcomponent rects
+  [{:keys [g scale style rect-type data]} owner]
+  (display-name [_] (str rect-type "-rects"))
   (render [_]
           [:g {:transform (data/translate g)}
-           (let [{:keys [padding width height]} scale
+           (let [{:keys [x-scale y-scale]} scale
                  {:keys [fill stroke]} style
-                 [ord-data num-data] (data/separate-data data)
-                 min-data (math/min-value num-data)
-                 max-data (math/max-value num-data)
-                 height-fn (lsc/linear-scale {:domain [min-data max-data]
-                                              :range-scale [0 height]})
-                 width-fn (osc/ordinal-scale {:domain ord-data
-                                              :range-bands [[0 width] padding]})
-                 z-bottom (height-fn 0)
-                 z-top (- height z-bottom)]
+                 width-creator (construct-width x-scale rect-type)
+                 height-creator (construct-height y-scale rect-type)
+                 x-creator (construct-x x-scale rect-type)
+                 y-creator (construct-y y-scale rect-type)]
              (om/build-all shape/rect
                            (->> data
-                                (map-indexed (fn [idx [ord-d num-d]]
-                                               (let [h (-> (height-fn num-d)
-                                                           (- z-bottom)
-                                                           math/->pos)]
-                                                 {:width (.rangeBand width-fn)
-                                                  :height h
-                                                  :x (width-fn ord-d)
-                                                  :y (if (neg? num-d)
-                                                       z-top
-                                                       (- z-top h))
-                                                  :fill fill
-                                                  :stroke stroke
-                                                  :react-key idx})))
-                                vec)
+                                (map-indexed (fn [idx d]
+                                               {:width (width-creator d)
+                                                :height (height-creator d)
+                                                :x (x-creator d)
+                                                :y (y-creator d)
+                                                :fill fill
+                                                :stroke stroke
+                                                :react-key idx})))
                            {:key :react-key}))]))
 
-(defcomponent vertical-bar-chart
-  "Creates a React vertical bar chart.
+(defn construct-scale
+  [rect-type min-data max-data ord-data padding width height]
+  (if (= "horizontal" rect-type)
+    {:x-scale (lsc/linear-scale {:domain [min-data max-data]
+                                 :range-scale [width 0]})
+     :y-scale (osc/ordinal-scale {:domain ord-data
+                                  :range-bands [[0 height] padding]})}
+    {:x-scale (osc/ordinal-scale {:domain ord-data
+                                  :range-bands [[0 width] padding]})
+     :y-scale (lsc/linear-scale {:domain [min-data max-data]
+                                 :range-scale [height 0]}) }))
 
-   Accept:
-
-   {:svg {:width <num>
-          :height <num>}
-   :rects {:padding <num>
-            :fill <string>
-           :stroke <string>}
-    :x-axis {:orient <string>
-            :line-axis {:show-line? <bool>
-                         :stroke <string>}
-            :each {:line {:x1 <num>
-                           :y1 <num>
-                          :x2 <num>
-                           :y2 <num>
-                          :stroke <string>}
-                    :text {:x <num>
-                          :y <num>
-                           :dx <num>
-                          :dy <num>
-                           :text-anchor <string>
-                          :content <string>}}
-    :y-axis {:orient <string>
-            :line-axis {:show-line? <bool>
-                         :stroke <string>}
-            :each {:line {:x1 <num>
-                           :y1 <num>
-                          :x2 <num>
-                           :y2 <num>
-                          :stroke <string>}
-                    :text {:x <num>
-                          :y <num>
-                           :dx <num>
-                          :dy <num>
-                           :text-anchor <string>
-                          :content <string>}
-             :rbd <num>}
-   :data <vector of map>}"
-   [{:keys [svg rects x-axis y-axis retriever-ks data]} owner]
-   (display-name [_] "vertical-bar-chart")
-   (render [_]
-           (let [{:keys [width height]} svg
-                 {:keys [padding fill stroke]} rects
-                 {:keys [ord-ks num-ks]} retriever-ks
-                 margin {:left  40
-                         :right 40
-                         :top 30
-                         :bottom 30}
-                 inner-size {:width (- width
-                                       (:left margin)
-                                       (:right margin))
-                             :height (- height
-                                        (:top margin)
-                                        (:bottom margin))}
-                 new-data (data/format-data data ord-ks num-ks)]
-             [:svg {:width width
-                    :height height}
-              (om/build vertical-rects
-                        {:g {:pos-x (:left margin)
-                             :pos-y (:top margin)}
-                         :scale {:padding padding
-                                 :width (:width inner-size)
-                                 :height (:height inner-size)}
-                         :style {:fill fill
-                                 :stroke stroke}
-                         :data new-data}
-                        {:react-key "vertical-rects"})
-              (let [{:keys [orient line-axis each]} x-axis
-                    {:keys [text line]} each]
-                (om/build axis/ordinal-x-axis
-                          {:scale {:width (:width inner-size)
-                                   :padding padding}
-                           :g {:pos-x (:left margin)
-                               :pos-y (if (= orient "top")
-                                        (:top margin)
-                                        (- height
-                                           (:bottom margin)))}
-                           :each {:text (into {:show-text? true
-                                               :dy (if (= orient "top")
-                                                     "-1.4em"
-                                                     "1.4em")
-                                               :text-anchor "middle"}
-                                              text)
-                                  :line (into {:y2 (if (= orient "top")
-                                                     "-0.5em"
-                                                     "0.5em")
-                                               :stroke "black"}
-                                              line)}
-                           :line-axis (into {:show-line? true
-                                             :stroke "black"}
-                                            line-axis)
-                           :data new-data}
-                          {:react-key "ordinal-x-axis"}))
-              (let [{:keys [orient line-axis each rbd]} y-axis
-                    {:keys [text line]} each]
-                (om/build axis/numerical-y-axis
-                          {:scale {:height (:height inner-size)
-                                   :rbd (if (number? rbd) rbd 20)}
-                           :g {:pos-x (if (= orient "right")
-                                        (- width
-                                           (:right margin))
-                                        (:left margin))
-                               :pos-y (:top margin)}
-                           :each {:text (into {:show-text? true
-                                               :text-anchor (if (= orient "right")
-                                                              "start"
-                                                              "end")
-                                               :dy "0.32em"
-                                               :x (if (= orient "right")
-                                                    9
-                                                    -9)}
-                                              text)
-                                  :line (into {:x2 (if (= orient "right")
-                                                     6
-                                                     -6)
-                                               :stroke "black"}
-                                              line)}
-                           :line-axis (into {:show-line? true
-                                             :stroke "black"}
-                                            line-axis)
-                           :data new-data}
-                          {:react-key "numerical-y-axis"}))])))
+(defcomponent bar-chart
+  [{:keys [svg rect-type rects-opts x-axis y-axis retriever-ks data]} owner]
+  (display-name [_] (str rect-type "-bar-chart"))
+  (render [_]
+          (let [{:keys [width height]} svg
+                {:keys [padding fill stroke]} rects-opts
+                {:keys [ord-ks num-ks]} retriever-ks
+                margin {:left  40
+                        :right 40
+                        :top 30
+                        :bottom 30}
+                container (data/inner-container width height margin)
+                new-data (data/format-data data ord-ks num-ks)
+                [ord-data num-data] (data/separate-data new-data)
+                min-data (math/min-value num-data)
+                max-data (math/max-value num-data)
+                {:keys [x-scale y-scale]} (construct-scale rect-type
+                                                           min-data
+                                                           max-data
+                                                           ord-data
+                                                           padding
+                                                           (:width container)
+                                                           (:height container))]
+            [:svg {:width width
+                   :height height}
+             (om/build rects
+                       {:g {:pos-x (:left margin)
+                            :pos-y (:top margin)}
+                        :scale {:x-scale x-scale
+                                :y-scale y-scale}
+                        :style {:fill fill
+                                :stroke stroke}
+                        :rect-type rect-type
+                        :data new-data}
+                       {:react-key "rects"})
+             (let [{:keys [orient line-axis end-text each]} x-axis]
+               (om/build ax/axis
+                         {:outer-container {:size svg
+                                            :margin margin}
+                          :each each
+                          :orient orient
+                          :scale {:scale-type "ordinal"
+                                  :scale-fn x-scale}
+                          :end-text end-text
+                          :line-axis line-axis
+                          :data (if (= rect-type "horizontal")
+                                  {:min-data min-data
+                                   :max-data max-data
+                                   :all-data num-data}
+                                  {:all-data ord-data})}
+                         {:react-key "x-axis"}))
+             (let [{:keys [orient line-axis end-text each ticks]} y-axis]
+               (om/build ax/axis
+                         {:outer-container {:size svg
+                                            :margin margin}
+                          :each each
+                          :orient orient
+                          :scale {:scale-type "numerical"
+                                  :scale-fn y-scale
+                                  :scale-ticks ticks}
+                          :end-text end-text
+                          :line-axis line-axis
+                          :data (if (= rect-type "horizontal")
+                                  {:all-data ord-data}
+                                  {:min-data min-data
+                                   :max-data max-data
+                                   :all-data num-data})}
+                         {:react-key "y-axis"}))])))
